3ffaceabda8664587bf06900828e6cb390cc178f
jespadas <julio.espadas@gmail.com>
2021-07-28 15:01:11 +0200

    selectors with filters

---
+++ src/selectors/GetHeroesByPublisher.js
@@ -0,0 +1,13 @@
+import { heroes } from '../data/heroes';
+
+export const getHeroesByPublisher = (publisher) => {
+
+    const validPublishers = ['DC Comics', 'Marvel Comics'];
+
+    if (!validPublishers.includes(publisher)) {
+        throw new Error(`Publisher "${ publisher }" it is not correct`);
+    }
+
+    return heroes.filter(hero => hero.publisher === publisher);
+
+}
