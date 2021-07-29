e30790b9d3e731413131d4caba261d4340ff392e
jespadas <julio.espadas@gmail.com>
2021-07-29 18:09:50 +0200

    refactoring, user name for style

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,7 +1,20 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, NavLink, useHistory } from 'react-router-dom'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const Navbar = () => {
+
+    const { user: { name }, dispatch } = useContext(AuthContext);
+
+    // React Router hook to useHistory (like useParams)
+    const history = useHistory();
+
+    const handleLogout = () => {
+        dispatch({ type: types.logout });
+        history.replace('/login');
+    };
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
 
@@ -47,14 +60,16 @@ export const Navbar = () => {
 
             <div className="navbar-collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink
-                        activeClassName="active"
-                        className="nav-item nav-link"
-                        exact
-                        to="/login"
+
+                    <span className="nav-item nav-link text-info">
+                        {name}
+                    </span>
+                    <button
+                        className="btn nav-item nav-link"
+                        onClick={handleLogout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
